daffec47b5b1f978de2f3fee9b562744f6aedad7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-31 20:26:26 -0300

    First Code Python

---
+++ Project.py
@@ -0,0 +1,17 @@
+print("Contas")
+print((9**4+2)*6-1)
+print(1+(2+2)/4)
+print(4**2/30)
+print((10%3)*(10**2)+1-(10*4)/2)
+
+print("Victor")
+
+a=4
+b=5
+c,d = 5,6
+print(a+b)
+print((2*a)*(3*b) )
+print(a+b+c)
+
+
+
