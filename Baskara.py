89f699cf9f4a0e5e961dc3e262dfb0a261d5d383
Victor rodrigues <vitorr.29@hotmail.com>
2023-09-03 16:52:44 -0300

    little changes

--- Baskara.py
+++ Baskara.py
@@ -1,12 +1,10 @@
 a = 1
-b = 2
-c = 1
-
+b = 9
+c = 14
 De = (b**2) - (4*a*c) #delta
-D = De ** 1/2 #delta ao quadrado
+D = De ** (1/2) #raiz de delta
 
 x1 = (-b + D)/(2*a)
 x2 = (-b - D)/(2*a)
 
 print("Os Resultados da Equação são :", x1, x2)
-
--- Promoçao.py
+++ Promoçao.py
@@ -1,11 +1,14 @@
-salario_ant = 1500
+salario_ant = 1900
 porcentagem = 15 / 100
 porcentagem1 = 5 / 100
+
 up = (salario_ant * porcentagem)
 salario_prom = salario_ant + up
 desconto = up - porcentagem1
 salario_des = salario_prom - desconto
-print("O aumento de salário foi de:", salario_prom)
+
+print('Seu salario era de:', salario_ant)
 print("Você obteve um aumento de","R$", up )
+print("O aumento de salário foi de:", salario_prom)
 print("Infelizmente você obteve 5% de deconto no valor de:", "R$", desconto )
 print("Seu salario atual é de", salario_des) # salarios
